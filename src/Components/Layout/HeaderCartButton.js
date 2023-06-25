ebf8006c3376813540a01e2ba77c242a30315b1f
Tarmo Liivamägi <liivamagi.tarmo@gmail.com>
2023-06-25 15:23:11 +0300

    added animation on cart button

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
-import { useContext } from 'react';
+import { useContext,useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 import styles from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 
 function HeaderCartButton (props) {
+    const [btnIsHighlighted,setBtnIsHighlighted]= useState(false);
    const cartCtx = useContext(CartContext);
    const numberOfCartItems = cartCtx.items.reduce((currentNum,item)=>{
     return currentNum + item.amount
    },0) 
+   
+   const {items } = cartCtx;
+   const btnStyles=`${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+   useEffect(()=>{
+    if(items.length === 0){
+        return;
+    }
+    const timer = setTimeout(()=>{
+        setBtnIsHighlighted(false)
+    },300);
+   
+    setBtnIsHighlighted(true);
+    return() => {
+        clearTimeout(timer)
+    }
+   },[items])
+
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={btnStyles} onClick={props.onClick}>
             <span className={styles.icon}><CartIcon/></span>
             <span>Your Cart</span>
             <span className={styles.badge}>{numberOfCartItems}</span>
