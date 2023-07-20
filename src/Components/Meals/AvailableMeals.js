180bffeb222fe0b341c869de4b0f40162adb7a45
Tarmo Liivamägi <liivamagi.tarmo@gmail.com>
2023-07-20 20:58:33 +0300

    error handling but state is not working properly

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -9,36 +9,62 @@ import styles from './AvailableMeals.module.css';
 
 function AvailableMeals() {
 const [meals,setMeals] = useState([]);
-const [isLoading,setIsLoading] = useState(true)
+const [isLoading,setIsLoading] = useState(true);
+const [httpError,setHttpError] = useState();
 
 const url = 'https://food-app-5b53c-default-rtdb.europe-west1.firebasedatabase.app/meals.json';
 
-const fetchMeals = async() => {
-  
-  const response = await fetch(url);
-  const jsonData = await response.json();
-  console.log(jsonData)
-  const loadedMeals = [];
-  for(const key in jsonData){
-    loadedMeals.push({
-      id:key,
-      name:jsonData[key].name, //jsonData[key]<--- võtab siis data kaustast key ja siis sealt .name-ga saad nime
-      description:jsonData[key].description,
-      price:jsonData[key].price,
-      key:key
-    })
+
+
+
+useEffect(() => {
+  const fetchMeals = async () => {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const responseData = await response.json();
+
+    const loadedMeals = [];
+
+    for (const key in responseData) {
+      loadedMeals.push({
+        id: key,
+        name: responseData[key].name,
+        description: responseData[key].description,
+        price: responseData[key].price,
+      });
+    }
+
+    setMeals(loadedMeals);
+    setIsLoading(false);
   };
-  setMeals(loadedMeals);
-  setIsLoading(false);
-};
+
+  fetchMeals().catch((error) => {
+    setIsLoading(false);
+    setHttpError(error.message);
+  });
+}, []);
 
 
-useEffect(()=> {
-   fetchMeals()
-},[])
 
-if(isLoading){
-  return <section className={styles.MealsLoading}><p>loading...</p></section>
+
+if (isLoading) {
+  return (
+    <section className={styles.MealsLoading}>
+      <p>Loading...</p>
+    </section>
+  );
+}
+
+if (httpError) {
+  return (
+    <section className={styles.MealsError}>
+      <p>{httpError}</p>
+    </section>
+  );
 }
   const mealsList = meals.map((meal) => (
     <MealItem
